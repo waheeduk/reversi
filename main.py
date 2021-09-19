eb39262219aafabe5041eb937beed6209ba36d1c
waheeduk <62504045+waheeduk@users.noreply.github.com>
2021-09-19 21:15:27 +0100

    Added an endgame where game detects remaining valid moves, and counts who won if there are no valid moves remaining

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 tile_size = 64 #size of individual grid piece
 turn = 1 #if turn is odd, black plays, else, white plays, black starts
+no_move_turns = 0
 
 #values for checking up down left right, and combinations in between
 directions = [ ]
@@ -25,6 +26,11 @@ directions.append(nw)
 directions.append(se)
 directions.append(sw)
 
+all_possible = [ ]
+for x in range(0, 8):
+    for y in range(0, 8):
+        all_possible.append(((x*tile_size), (y*tile_size)))
+
 #colours for the pieces
 black = (0,0,0)
 white = (255, 255, 255)
@@ -35,10 +41,35 @@ def pos_to_tile(pos):
     y = (pos[1] // tile_size) * tile_size
     return (x, y)
 
+def valid_moves_available():
+    """vchecks how many valid remaining moves are possible"""
+    valid_moves = 0
+    valid_move_list = []
+    for n in range(0, len(enemy_piece)):
+        for x in range(0, len(directions)):
+            checking_piece = ((enemy_piece[n][0] + directions[x][0]), (enemy_piece[n][1] + directions[x][1]))
+            if checking_piece in enemy_piece or checking_piece in current_piece:
+                continue
+            if checking_piece[0] > (7*tile_size) or checking_piece[0] < 0:
+                continue
+            if checking_piece[1] < 0 or checking_piece[1] > (7*tile_size):
+                continue
+            else:
+                valid_move_list.append(checking_piece)
+                valid_moves += 1
+    if valid_moves == 0:
+        return False
+    else:
+        print(f'for the turn {turn}, there are {valid_moves} moves available\n')
+        print(valid_move_list)
+        return True
+
 def check_valid(position):
     """checks in every direction listed above, to see if the player has selected a tile
     adjacent to an enemy piece"""
     hits = 8
+    if position in enemy_piece or position in current_piece:
+        return False
     for n in range(0, len(directions)):
         if ((position[0] + directions[n][0]), (position[1] + directions[n][1])) in enemy_piece:
             flip(position, ( (position[0] + directions[n][0]), (position[1]+directions[n][1]) ) )
@@ -72,6 +103,14 @@ def flip_back(start, interval):
 		current_piece.append((start[0], start[1]))
 		return flip_back((start[0] + interval[0], start[1] + interval[1]), interval)
 
+def result():
+    if len(black_occupied) > len(white_occupied):
+        print(f'black won with {len(black_occupied)} pieces to white\'s {len(white_occupied)} pieces')
+    elif len(white_occupied) > len(black_occupied):
+        print(f'white won with {len(white_occupied)} pieces to black\'s {len(black_occupied)} pieces')
+    else:
+        print('it\'s a draw')
+
 #initialise pygame module
 pygame.init()
 screen=pygame.display.set_mode((8*tile_size,8*tile_size))
@@ -101,6 +140,14 @@ while True:
         if event.type==pygame.QUIT: #exit
             pygame.quit()
             sys.exit()
+        if no_move_turns == 2:
+            result()
+        elif valid_moves_available() == False:
+            no_move_turns += 1
+            turn += 1
+            print('no valid moves')
+        else:
+            print('valid moves')
         if event.type==pygame.MOUSEBUTTONUP:
             mouse_pos = pygame.mouse.get_pos()
             tile_pos = pos_to_tile(mouse_pos)
@@ -113,6 +160,7 @@ while True:
                 else:
                     current_piece = white_occupied
                     enemy_piece = black_occupied
+                no_move_turns = 0
     for n in range(0, len(black_occupied)): #draws black pieces
         pygame.draw.circle(screen, black, ((black_occupied[n][0] + 32), black_occupied[n][1] + 32), 25 )
     for n in range(0, len(white_occupied)): #draws white pieces
